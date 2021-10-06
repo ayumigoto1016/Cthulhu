21b20153094930e6c8f4fb2afc1bc10896e23fe6
EC2 Default User <ec2-user@ip-172-31-30-194.ap-northeast-1.compute.internal>
2021-10-06 14:27:57 +0000

    add roulettes

--- main.js
+++ main.js
@@ -1,11 +1,28 @@
-const box = document.getElementById("box");
-const roulette = document.getElementById("roulette");
-
-const showResult = (show) =>{
-  box.textContent = show;
+const box1 = document.getElementById("box1");
+const roulette1 = document.getElementById("roulette1");
+const box2 = document.getElementById("box2");
+const roulette2 = document.getElementById("roulette2");
+const box3 = document.getElementById("box3");
+const roulette3 = document.getElementById("roulette3");
+const showResult1 = (show) =>{
+  box1.textContent = show;
+};
+const showResult2 = (show) =>{
+  box2.textContent = show;
+};
+const showResult3 = (show) =>{
+  box3.textContent = show;
 };
 
-roulette.addEventListener('click', ()=>{
-  const result = Math.floor(Math.random()*10)+1;
-  showResult(result);
+roulette1.addEventListener('click', ()=>{
+  const result = Math.floor(Math.random()*6)+1;
+  showResult1(result);
+});
+roulette2.addEventListener('click', ()=>{
+  const result = Math.floor(Math.random()*11)+2;
+  showResult2(result);
+});
+roulette3.addEventListener('click', ()=>{
+  const result = Math.floor(Math.random()*16)+3;
+  showResult3(result);
 });
